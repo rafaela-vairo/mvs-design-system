7564d3c10809eda3e1aabc69d89d608db62b247a
Guilherme Vairo <guilhermevairo@localhost.localdomain>
2019-10-14 16:07:08 -0300

    Progresso em mvs-card-expander

--- src/components/mvs-card-expander/mvs-card-expander.tsx
+++ src/components/mvs-card-expander/mvs-card-expander.tsx
@@ -1,4 +1,4 @@
-import { Component, h, Prop } from '@stencil/core'
+import { Component, h, Prop, Element } from '@stencil/core'
 
 @Component({
 	tag: 'mvs-card-expander',
@@ -10,37 +10,52 @@ export class MvsCardExpander {
 	@Prop() cardtitle: string
 	@Prop() subtitle: string
 	@Prop() text: string
-	@Prop() button: string
 	@Prop() link: string = 'http://google.com'
 	@Prop() media: string =
 		'https://material-ui.com/static/images/cards/contemplative-reptile.jpg'
 	@Prop() alt: string = 'Texto alternativo em falta, favor entrar em contato'
-	@Prop() width: number
-	@Prop() open: 'open' | 'closed' = 'closed'
+	@Element() el: HTMLElement
+	/* @Event() transitioned: EventEmitter
+	transitionedEmit(transition) {
+		this.transitioned.emit(transition)
+	}
+	
+	@Listen('')
+	transitionedListen() {
+		() => this.animateToNextState()) this.animateToNextState()
+	} */
+
+	currentState = 0
+	states = [
+		() => (this.el.style.transform = 'translate(0px, 0px)'),
+		() => (this.el.style.transform = 'translate(100px, 0px)'),
+		() => (this.el.style.transform = 'translate(100px, 100px)'),
+		() => (this.el.style.transform = 'translate(0px, 100px)'),
+	]
+
+	animateToNextState = () => {
+		requestAnimationFrame(() => {
+			this.states[++this.currentState % this.states.length]()
+		})
+	}
 
 	render() {
 		return (
-			<div>
-				<div class='root' tabindex='0'>
-					<div class='media-container'>
-						<img class='media' src={this.media} alt={this.alt} />
+			<div class='item' tabindex='0'>
+				<div class='cover'></div>
+				<div class='content'>
+					<div>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+							euismod mauris sed mi dapibus posuere. Integer libero augue,
+							tincidunt eget mauris et, congue tincidunt mauris. Etiam eu quam
+							eu velit pharetra vestibulum. Nullam id nibh dui. Fusce a nibh a
+							nunc scelerisque fermentum luctus eget ex. Proin erat dolor,
+							dapibus id laoreet quis, tristique ac risus. Class aptent taciti
+							sociosqu ad litora torquent per conubia nostra, per inceptos
+							himenaeos. Nulla gravida risus venenatis massa elementum gravida.
+						</p>
 					</div>
-					<div class='main'>
-						<div class='text'>
-							<mvs-overline>{this.overline}</mvs-overline>
-							<mvs-title level='t5'>
-								<a href={this.link}>{this.cardtitle}</a>
-							</mvs-title>
-							<mvs-subtitle type='two'>{this.subtitle}</mvs-subtitle>
-							<mvs-paragraph bodytwo>{this.text}</mvs-paragraph>
-						</div>
-						<div class='buttons'>
-							<mvs-button>{this.button}</mvs-button>
-						</div>
-					</div>
-				</div>
-				<div class={`${this.open}`}>
-					<slot />
 				</div>
 			</div>
 		)
