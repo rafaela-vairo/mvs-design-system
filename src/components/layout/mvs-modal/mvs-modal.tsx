3e3686c6f4f59747fb944c58634e2b3421ed0c14
renanlima <renanlima@tic.ufrj.br>
2019-10-29 16:58:13 -0300

    Correção dos Break Points da Grid

--- src/components/layout/mvs-modal/mvs-modal.tsx
+++ src/components/layout/mvs-modal/mvs-modal.tsx
@@ -8,15 +8,19 @@ import { Component, h, Prop } from '@stencil/core';
 })
 export class MvsModal {
 
-    @Prop() show: Boolean;
+    @Prop() show: Boolean = false;
 
     hide(e) {
         e.target.classList.remove('show');
     }
 
+    componentDidLoad() {
+        console.log(this.show)
+    }
+
     render() {
         return (
-            <div class="dialog show" onClick={this.hide.bind(this)}>
+            <div class={'dialog ' + (this.show ? 'show' : '')} onClick={this.hide.bind(this)}>
                 <div class="modal open">
                     <div class="header">
                         <button id="close"></button>
