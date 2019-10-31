00ae830fc91a5f9c6c426327bacc3fde00f02cb3
Renan Ferreira de Lima <renanferreiradelima98@live.com>
2019-10-31 15:12:13 -0300

    Correçao do modal

--- src/components/layout/mvs-modal/mvs-modal.tsx
+++ src/components/layout/mvs-modal/mvs-modal.tsx
@@ -9,22 +9,31 @@ import { Component, h, Prop } from '@stencil/core';
 export class MvsModal {
 
     @Prop({ reflect: true, reflectToAttr: true }) name: string;
-    @Prop() show: boolean;
+		@Prop() show: boolean;
+
+		// @Listen('showModal', { capture: true })
+		// showModal(ev) {
+		// 	console.log(ev);
+		// }
 
     hide(e) {
-        if (e.target.offsetParent.id === this.name) {
-            e.target.offsetParent.classList.remove('show');
-            return;
-        }
-        e.target.classList.remove('show');
+			console.log(e);
+			if (e.target.offsetParent === null) {
+				e.target.classList.remove('show');
+			} else if (e.target.id === 'close') {
+				e.target.offsetParent.classList.remove('show');
+			}
+			// if (e.target.offsetParent.id === this.name) {
+			// 	return;
+			// }
     }
 
     render() {
         return (
-            <div id={this.name} class={'dialog ' + (this.show ? 'show' : '')} onClick={this.hide.bind(this)}>
+            <div id={this.name} class={'dialog ' + (this.show ? 'show' : '')} onClick={(e) => this.hide(e)}>
                 <div class="modal open">
                     <div class="header">
-                        <button id="close" onClick={this.hide.bind(this)}></button>
+											<button id="close" onClick={this.hide.bind(this)}></button>
                     </div>
                 </div>
             </div>
